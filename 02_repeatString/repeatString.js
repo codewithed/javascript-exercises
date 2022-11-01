af06cb49c2ef8cbd6b647e78a9b244347bb4f3f0
Edmund Debrah <edmundking05@gmail.com>
2022-11-01 17:24:45 +0000

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
-};
+function repeatString(string, num) {
+    let res = "";
+    if (num === 0 || string.length === 0) {
+        return "";
+    }
+    if (num < 0)
+        return "ERROR";
+    for (let j = 0; j < num; j++)
+    {
+        res += string;
+    }
+
+    return res;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
