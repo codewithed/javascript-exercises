55e7f087c0f6cfaf966dffe9234cc42e08a738fa
Edmund Debrah <edmundking05@gmail.com>
2022-11-02 14:40:35 +0000

    complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr = arr;
 
+    for (let i = 0; i < newArr.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (args.includes(newArr[i]))
+                newArr.splice(i, 1);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
