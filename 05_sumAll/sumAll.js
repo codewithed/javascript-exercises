1ef4e8c991c0bdc34099050a4fc76493213d812f
Edmund Debrah <edmundking05@gmail.com>
2022-11-02 15:04:57 +0000

    complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    let sum = 0;
+    if (start >= 0 && finish >= 0) {
+        if (start <= finish) {
+            for (let i = start; i <= finish; i++) {
+             sum += i;
+            }
+        }
+        else if (start > finish) {
+            for (let i = start; i >= finish; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    else
+        return ERROR;
 };
 
 // Do not edit below this line
