360a3fd2578a608513da466961655886f8e8092d
Edmund Debrah <edmundking05@gmail.com>
2022-11-02 22:48:39 +0000

    complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  res = (tempF - 32) * (5/9);
+  res = parseFloat(res.toFixed(1));
+  return res;
 };
 
-const ctof = function() {
 
+const ctof = function(tempC) {
+  let res = tempC * (9/5) + 32;
+  res = parseFloat(res.toFixed(1));
+  return res;
 };
 
 // Do not edit below this line
