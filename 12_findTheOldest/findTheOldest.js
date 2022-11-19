a89bc7f2595007e0dac6ddb8165741f9e4bb7307
Edmund Debrah <edmundking05@gmail.com>
2022-11-19 18:45:37 +0000

    complete all exercises up to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    const date = new Date();
+
+    // add ages to each item
+    arr.forEach(item => {
+        if (!item.yearOfDeath)
+            item.age = date.getFullYear() - item.yearOfBirth;
+        else 
+            item.age = item.yearOfDeath - item.yearOfBirth;
+    });
+
+    // get the oldest age
+    const oldest = arr.reduce((currentOldest, item) => {
+        if (item.age > currentOldest)
+            currentOldest = item.age;
+        return currentOldest;
+    }, 0);
+
+    const res = arr.find((item) => {
+        return item.age === oldest;
+    });
+
+    return res;
 
 };
 
